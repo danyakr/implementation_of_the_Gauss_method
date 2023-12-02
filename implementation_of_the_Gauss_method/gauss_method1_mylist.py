3bac19b1fc5e7947838e3f8c64c1f28641a214cc
danyakr <75899583+danyakr@users.noreply.github.com>
2023-12-02 17:44:10 +0300

    init

---
+++ implementation_of_the_Gauss_method/gauss_method1_mylist.py
@@ -0,0 +1,43 @@
+
+import sys
+from matrix_io import MyList, print_update_matrix, do_update_choice, updated_matrix
+
+
+matrix, n = do_update_choice(updated_matrix)
+
+# Проходим по всем строкам кроме последней
+for i in range(1, n):
+    lead_element = matrix[i][i]  # Вычисляем ведущий элемент для текущей строки
+    # Делим всю текущую строку на ведущий элемент (приведение к единичному элементу на главной диагонали)
+    for j in range(i, (n + 1) + 1):
+        matrix[i][j] = round(matrix[i][j] / lead_element, 6)
+
+    # Проходим по оставшимся строкам
+    for k in range(i + 1, n + 1):
+        conversion_factor = matrix[k][i]  # Коэффициент преобразования для вычитания
+        # Вычитаем из текущей строки произведение строки с коэффициентом преобразования и ведущей строкой
+        for j in range(i, (n + 1) + 1):
+            matrix[k][j] = round(matrix[k][j] - matrix[i][j] * conversion_factor, 6)
+
+print_update_matrix(matrix)
+
+ans = MyList([0] * n)  # Создаем список для решения системы
+
+try:
+    ans[n] = round(matrix[n][n + 1] / matrix[n][n], 6)  # Находим значение последнего корня
+except ZeroDivisionError:
+    print('Матрица либо несовместна, либо совместна и неопределена')
+    sys.exit()
+
+# Обратный ход для нахождения остальных неизвестных
+for i in range(n - 1, 0, -1):
+    amount = 0
+    # Проходим от диагонального элемента не включительно до конца строки
+    for j in range(i + 1, n + 1):
+        # Вычисляем сумму произведений коэффициентов и найденных значений для неизвестных
+        amount += matrix[i][j] * ans[j]
+    # Находим значение неизвестных
+    ans[i] = round(matrix[i][n + 1] - amount, 6)
+
+print(f'Решение {ans}')
+
